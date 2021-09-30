2735f6ae4689880817e311923e839bd729aeb17a
Gisha <spaceshhz@gmail.com>
2021-09-30 18:23:56 +0300

    build scene for facade pattern

---
+++ Patterns-Practice/Assets/_Projects/Facade Pattern/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Gisha.PatternsPractice.FacadePattern
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 10f;
+        
+        private Vector3 _input;
+        
+        private Rigidbody _rigidbody;
+        private Animator _animator;
+        
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            _input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            _rigidbody.velocity = _input.normalized * moveSpeed;
+
+            if (_input.magnitude > 0)
+            {
+                _rigidbody.rotation = Quaternion.LookRotation(_input);
+                _animator.SetBool("IsRunning", true);
+            }
+            else
+                _animator.SetBool("IsRunning", false);
+        }
+    }
+}
